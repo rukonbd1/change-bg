561bd6df778950bb025a9f752c6f492862909c23
rukonbd1 <rukonbd.dev@gmail.com>
2022-09-19 08:38:52 +0600

    font added

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("Hello World");
-let s= 67;
+//Color code generator
+const randomCode=()=>{
+    const codes="123456789abcdef";
+    let colorCode = "";
+    for (let i = 0; i < 6; i++) {
+        const code = Math.floor(Math.random()*15)
+        colorCode+=codes.substring(code,code+1);
+    }
+    return colorCode;
+}
+//Adding click event
+document.getElementById("btn").addEventListener("click",()=>{
+    const newColor = randomCode();
+    document.getElementById("color_code").innerText="#"+newColor;
+    document.getElementById("body").style.backgroundColor="#"+newColor;
+})
